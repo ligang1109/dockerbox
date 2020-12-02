5e2bd7f57ca6f97d5ea7890cf3c40cb6a4747840
gibsonli <gibsonli@tencent.com>
2020-12-02 10:03:48 +0800

    update

--- src/go.mod
+++ src/go.mod
@@ -4,7 +4,7 @@ go 1.13
 
 require (
 	github.com/goinbox/color v1.0.0 // indirect
-	github.com/goinbox/golog v2.1.2+incompatible
+	github.com/goinbox/golog v2.2.0+incompatible
 	github.com/goinbox/gomisc v1.2.0
 	github.com/goinbox/shell v1.0.1
 )
--- src/main/dbox/main.go
+++ src/main/dbox/main.go
@@ -18,7 +18,7 @@ func main() {
 	var dconfPath string
 
 	fs := flag.NewFlagSet(os.Args[0], flag.ExitOnError)
-	fs.IntVar(&logLevel, "logLevel", golog.LEVEL_INFO, "global log level")
+	fs.IntVar(&logLevel, "logLevel", golog.LevelInfo, "global log level")
 	fs.StringVar(&dconfPath, "dconfPath", os.Getenv("HOME")+"/.dconf.json", "dbox conf path")
 	fs.Parse(os.Args[1:])
 
@@ -29,31 +29,31 @@ func main() {
 	if dconfPath == "" {
 		logger.Error([]byte("missing flag dconfPath"))
 		flag.PrintDefaults()
-		os.Exit(errno.E_SYS_INVALID_DCONF)
+		os.Exit(errno.ESysInvalidDconf)
 	}
 	if !gomisc.FileExist(dconfPath) {
 		logger.Error([]byte("dconfPath not exist: " + dconfPath))
-		os.Exit(errno.E_SYS_INVALID_DCONF)
+		os.Exit(errno.ESysInvalidDconf)
 	}
 	logger.Debug([]byte("dconfPath: " + dconfPath))
 
 	err := dconf.Init(dconfPath)
 	if err != nil {
 		logger.Error([]byte("dconf init error: " + err.Error()))
-		os.Exit(errno.E_SYS_INVALID_DCONF)
+		os.Exit(errno.ESysInvalidDconf)
 	}
 
 	fargs := fs.Args()
 	if len(fargs) == 0 {
 		logger.Error([]byte("do not has cmd arg"))
-		os.Exit(errno.E_DBOX_INVALID_CMD)
+		os.Exit(errno.EDboxInvalidCmd)
 	}
 
 	name := strings.TrimSpace(fargs[0])
 	command := cmd.NewCommandByName(name)
 	if command == nil {
 		logger.Error([]byte("unknown cmd: " + name))
-		os.Exit(errno.E_DBOX_INVALID_CMD)
+		os.Exit(errno.EDboxInvalidCmd)
 	}
 
 	command.Run(fargs[1:], logger)
